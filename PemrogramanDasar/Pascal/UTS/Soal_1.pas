badb8b5495dff0d58976ec5e8df596aa1737f37d
Fittra Ferdiansyah M.Hc <penyalahgunaanakun@gmail.com>
2024-10-27 21:18:19 +0700

    Create Soal_1.pas

---
+++ PemrogramanDasar/Pascal/UTS/Soal_1.pas
@@ -0,0 +1,11 @@
+program UTS_Soal_1;
+uses crt;
+var nama: string;
+
+begin
+    clrscr;
+    write('Masukkan nama Anda: ');
+    read(nama);
+    writeln('Selamat datang ', nama);
+    readln;
+end.
